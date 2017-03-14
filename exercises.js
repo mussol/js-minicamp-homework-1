54231db4529af39b8de57e1a11cad7038591e068
mussol <gemma.valls@gmail.com>
2017-03-15 00:09:16 +0100

    Completed homework-1

--- exercises.js
+++ exercises.js
@@ -2,138 +2,152 @@
 
 function multiplyByTen(num) {
   //return num after multiplying it by ten
-  //code here
+  num *= 10;
+  return num;
 }
 
 function subtractFive(num) {
   //return num after subtracting five
-  //code here
+  num -= 5;
+  return num;
 }
 
 function areSameLength(str1, str2) {
   //return true if the two strings have the same length
   //otherwise return false
-  //code here
+  return (str1.length === str2.length);
 }
 
 function areEqual(x, y) {
   //return true if x and y are the same
   //otherwise return false
-  //code here
+  return (x === y);
 }
 
 function lessThanNinety(num) {
   //return true if num is less than ninety
   //otherwise return false
-  //code here
+  return (num < 90);
 }
 
 function greaterThanFifty(num) {
   //return true if num is greater than fifty
   //otherwise return false
-  //code here
+  return (num > 50);
 }
 
 function add(x, y) {
   //add x and y together and return the value
-  //code here
+  var result = x + y;
+  return result;
 }
 
 function subtract(x, y) {
   //subtract y from x and return the value
-  //code here
+  var result = x - y;
+  return result;
 }
 
 function divide(x, y) {
   //divide x by y and return the value
-  //code here
+  var result = x / y;
+  return result;
 }
 
 function multiply(x, y) {
   //multiply x by y and return the value
-  //code here
+  var result = x * y;
+  return result;
 }
 
 function getRemainder(x, y) {
   //return the remainder from dividing x by y
-  //code here
+  var result = x % y;
+  return result;
 }
 
 function isEven(num) {
   //return true if num is even
   //otherwise return false
-  //code here
+  return (num % 2 == 0);
 }
 
 function isOdd(num) {
   //return true if num is false
   //otherwise return false
-  //code here
+  return (num % 2 != 0);
 }
 
 function square(num) {
   //square num and return the new value
-  //code here
+  var result = Math.pow(num, 2);
+  return result;
 }
 
 function cube(num) {
   //cube num and return the new value
-  //code here
+  var result = Math.pow(num, 3);
+  return result;
 }
 
 function raiseToPower(num, exponent) {
   //raise num to whatever power is passed in as exponent
-  //code here
+  var result = Math.pow(num, exponent);
+  return result;
 }
 
 function roundNumber(num) {
   //round num and return it
-  //code here
+  return Math.round(num);
 }
 
 function roundUp(num) {
   //round num up and return it
-  //code here
+  return Math.ceil(num);
 }
 
 function addExclamationPoint(str) {
   //add an exclamation point to the end of str and return the new string
   //'hello world' -> 'hello world!'
-  //code here
+  return str + '!';
 }
 
 function combineNames(firstName, lastName) {
   //return firstName and lastName combined as one string and separated by a space.
   //'Lambda', 'School' -> 'Lambda School'
-  //code here
+  return firstName + ' ' + lastName;
 }
 
 function getGreeting(name) {
   //Take the name string and concatenate other strings onto it so it takes the following form:
   //'Sam' -> 'Hello Sam!'
-  //code here
+  return 'Hello ' + name + '!';
 }
 
 //If you can't remember these area formulas then head over to Google or look at the test code.
  
 function getRectangleArea(length, width) {
   //return the area of the rectangle by using length and width
-  //code here
+  var rectangleArea = length * width;
+  return rectangleArea;
 }
 
 function getTriangleArea(base, height) {
   //return the area of the triangle by using base and height
-  //code here
+  var triangleArea = base * height / 2;
+  return triangleArea;
 }
 
 function getCircleArea(radius) {
   //return the rounded area of the circle given the radius
-  //code here
+  var circleArea = Math.round(radius * radius * Math.PI);
+  return circleArea;
 }
 
 function getRectangularPrismVolume(length, width, height) {
   //return the volume of the 3D rectangular prism given the length, width, and height
-  //code here
+  var rectPrismVolume = length * width * height;
+  return rectPrismVolume;
 }
 
 //Do not modify code below this line.
